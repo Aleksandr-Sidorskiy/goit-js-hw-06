e0587fd994e4121395191a2d127dfc347c4a51ff
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-04-13 21:49:27 +0300

    Update hw1.js
    виконав завдання №2

--- js/hw1.js
+++ js/hw1.js
@@ -1,26 +1,43 @@
 // =========завдання №1=========
 
-const navEl = document.querySelectorAll('.item');
-console.log('Number of categories:', navEl.length);
+// const navEl = document.querySelectorAll('.item');
+// console.log('Number of categories:', navEl.length);
 
-const navElAnimal = document.querySelector('.animals');
-const navElAnimalLength = navElAnimal.children.length;
-console.log(`Category: Animals
-Elements:${navElAnimalLength}`);
+// const navElAnimal = document.querySelector('.animals');
+// const navElAnimalLength = navElAnimal.children.length;
+// console.log(`Category: Animals
+// Elements:${navElAnimalLength}`);
 
-const navElProducts = document.querySelector('.products');
-const navElProductsLength = navElProducts.children.length;
-console.log(`Category: Products
-Elements:${navElProductsLength}`);
+// const navElProducts = document.querySelector('.products');
+// const navElProductsLength = navElProducts.children.length;
+// console.log(`Category: Products
+// Elements:${navElProductsLength}`);
 
-const navElTechnologies = document.querySelector('.technologies');
-const navElTechnologiesLength = navElTechnologies.children.length;
-console.log(`Category: Technologies
-Elements:${navElTechnologiesLength}`);
+// const navElTechnologies = document.querySelector('.technologies');
+// const navElTechnologiesLength = navElTechnologies.children.length;
+// console.log(`Category: Technologies
+// Elements:${navElTechnologiesLength}`);
+
+// =========завдання №2=========
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const addLi = document.querySelector('#ingredients');
+
+const addElement = ingredients.map(text => {
+     
+  const  addItem = document.createElement('li');
+    addItem.classList.add("item");
+    addItem.textContent = text; 
+    return addItem;
+});
 
 
-
-
-
-
-
+console.log(addLi);
+addLi.append(...addElement);
