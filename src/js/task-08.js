1871f6c33a1748458f6dfeeab63a48ddb1d55731
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-04-26 20:18:59 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,12 @@
-
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+  console.log(`email: ${email.value},Password: ${password.value}`);
+  event.currentTarget.reset();
+});
