ac28faee897d37c618f699c27601476fb1a5da67
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-04-26 20:19:01 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
+
+const widgetEl = document.querySelector(".widget");
+const сhangeColorEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+сhangeColorEl.addEventListener("click", onApplicationBackgraundColor);
+
+function onApplicationBackgraundColor() {
+  // document.body.style.backgroundColor = getRandomHexColor(); изменяет фон на body
+  widgetEl.style.backgroundColor = getRandomHexColor();
+  return colorEl.textContent = getRandomHexColor()
+  
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
