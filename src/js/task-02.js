d298289dd8c41f5caef7808e49d66c52ed7c8558
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-04-26 20:18:43 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ const addLi = document.querySelector('#ingredients');
+
+const addElement = ingredients.map(text => {
+     
+  const  addItem = document.createElement('li');
+    addItem.classList.add("item");
+    addItem.textContent = text; 
+    return addItem;
+});
+console.log(addLi);
+addLi.append(...addElement);
