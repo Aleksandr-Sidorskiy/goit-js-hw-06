8813572dc1807d4708722beb5c29cb2c59162ce9
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-04-27 21:37:26 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,9 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener('input', (event) => {
-  if (inputEl.value == "") {
-   return outputEl;
-  } 
   console.log(inputEl.textContent);
-    outputEl.textContent = event.currentTarget.value;
+  outputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    outputEl.textContent = "Anonymous";
+  } 
 });
