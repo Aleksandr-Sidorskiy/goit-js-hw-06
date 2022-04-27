664108198e6b609ea1ed9adc79a0deed0d6cff9b
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-04-27 21:44:53 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@
 
 validationInputEl.addEventListener("blur", () => {
   console.log(validationInputEl.value.length);
-  if (validationInputEl.value.length == validationInputEl.dataset.length) { 
+  if (validationInputEl.value.length === validationInputEl.dataset.length) { 
     validationInputEl.classList.remove('invalid');
     validationInputEl.classList.add('valid');
   } else {
